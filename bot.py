34a466b5d76374104ba9004467b3896733519302
GaMeNu <98153342+GaMeNu@users.noreply.github.com>
2022-10-05 23:52:42 +0300

    Comments, +tips
    added a new command, and commented my code.
    
    suckage--
    print(f'Suckage level is now {suckage}')
    >>> Suckage level is now 999999999

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ import time as t
 import random as rand
 import datetime
 
-
+#All .env tokens, required for the bot to function without any modifications.
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
@@ -30,18 +30,22 @@ def reset_vars():
     global dead_members
     dead_members=[]
 
+#Time since connection established loop
 @tasks.loop(seconds=1)
 async def time_connected_loop():
     global uptime
-    await bot.change_presence(activity=discord.Activity(name='Watching for +help',details=f'Minutes online: 0'))
+    #How the hell do presences work
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name='for +help; Online for 0 minutes.',state='Existing',details=f'Hello world!'))
     uptime=0
+    #the update loop itself
     while True:
         await asyncio.sleep(1)
-        if (uptime%60==0):
-            await bot.change_presence(activity=discord.Activity(name='Watching for +help',details=f'Minutes online: {uptime//60}'))
         uptime+=1
+        if (uptime%60==0):
+            await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching,name=f'for +help; Online for {uptime//60} minutes.',details=f'Hello world!'))
 
 
+#Prints some data to terminal, also sends connected msg on COMMANDS_CHANNEL
 @bot.event
 async def on_ready():
     guild = discord.utils.find(lambda g: g.name == GUILD, bot.guilds)
@@ -59,6 +63,7 @@ async def on_ready():
     print("Started uptime loop!")
 
 
+#detects if a message was sent in NSFW_CHANNEL, and sends a "spotted" alert in ALERTS_CHANNEL
 @bot.event
 async def on_message(message):
     if (message.author==bot.user):
@@ -71,14 +76,39 @@ async def on_message(message):
     
     await bot.process_commands(message)
 
+#help command, pretty basic embed.
 @bot.command()
 async def help(ctx):
     embed_color=rand.randrange(1, 16777216)
     help_embed = discord.Embed(title="BWUBot help", description="Here you can find all currently available commands!",color=embed_color,timestamp=datetime.datetime.utcnow())
     help_embed.add_field(name="+help",value="Guess what this one does, I bet you can't.", inline=False)
     help_embed.add_field(name="+on?",value="Also works with: +status, +a?\nSends a response if the bot is on, along with the time it's been connected",inline=False)
-    help_embed.add_field(name="+kill [Message including a user(s) mention]",value="Kills the user(s) mentioned in the message",inline=False)
+    help_embed.add_field(name='+tips',value='Sends a random anxiety tip.',inline=False)
+    help_embed.add_field(name="+kill [Message including a user(s) mention]",value="Kills the alive user(s) mentioned in the message",inline=False)
+    help_embed.add_field(name="+revive [Message including a user(s) mention]",value="Revives the dead user(s) mentioned in the message",inline=False)
+    help_embed.add_field(name="+dead",value="Sends a list of all dead members.",inline=False)
+
+    
     await ctx.channel.send(embed=help_embed)
+
+
+#V0C4LO1D's command idea
+#Sends a random anxiety tip
+@bot.command()
+async def tips(ctx):
+    tiplist=['When anxiety starts kicking in, count backwards repeatedly from 10 in your head.',
+    'Close your eyes. Trust me, whenever you are trying to cope just close your eyes.',
+    'Drink water. Focus on the feeling of it traveling down your throat, and into your chest.',
+    'Fiddle with whatever resources you have around you',
+    'Sing a song in your head. Distract yourself from what\'s going on around you. If it helps, cover your ears and distract from reality.']
+
+    tip = 'Random anxiety tip by Charely:\n'
+
+    tip+=rand.choice(tiplist)
+
+    await ctx.channel.send(tip)
+
+#Sends a message if the pot is online, along with the time since the connection was established.
 @bot.command(name="on?", aliases = ['a?','status','uptime'])
 async def isOn(ctx):
     uptimeS = uptime%60
@@ -86,22 +116,27 @@ async def isOn(ctx):
     uptimeH = uptime//3600
     await ctx.send(f"Hello world! I am currently online.\nTime since connected: {uptimeH}:{uptimeM}:{uptimeS}")
 
+#Kill command, will go into more detail in the command
 @bot.command()
 async def kill(ctx):
-    
+    #Collects all mentions in message. If no mentions found returns a sort of error.
     mentions = ctx.message.mentions
     if not mentions:
         await ctx.send('Please mention member(s) to kill with an \'@\'')
         return
     for member in mentions:
+
+        #Protects the creator (me) and other bots.
         if member.name == 'GM' and not f'{ctx.message.author.name}#{ctx.message.author.discriminator}' == 'GM#2372':
             await ctx.send('Please do not attempt to murder my creator.')
             return
         if member.bot:
             await ctx.send('Bots are immune to murder.')
             return
-
+    
+    #Sends a killed message with a few checks to check if there are multiple members, specifically who to kill and who is already dead, and if the member killed himself.
     killed ='Congrats. '
+    #Multi person murder.
     if len(mentions)>=2:
         for member in mentions[0:-2]:
             if(not member in dead_members):
@@ -122,7 +157,7 @@ async def kill(ctx):
         else:
             killed+= f'{mentions[-1]} was already dead. The rest were killed.'
 
-    
+    #One person murder.
     else:
         if(not mentions[0] in dead_members):
             dead_members.append(mentions[0])
@@ -142,6 +177,7 @@ async def kill(ctx):
 @bot.command()
 async def revive(ctx):
 
+    #Kinda works like +kill, except the opposite for the checks with dead_members.
     mentions = ctx.message.mentions
     if not mentions:
         await ctx.send('Please mention member(s) to revive with an \'@\'')
@@ -189,13 +225,14 @@ async def revive(ctx):
 
     await ctx.channel.send(revived)
     
-
+#Sends a list of all members in dead_members
 @bot.command()
 async def dead(ctx):
     dead = "Dead members (L):"
     for member in dead_members:
         dead+=f'\n - {member}'
     await ctx.send(dead)
+
 
 
 @bot.command()
